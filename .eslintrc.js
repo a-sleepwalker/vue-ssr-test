978dcc543e28daf7a225a76e2abb6dddb8439872
taowt <msuserting@hotmail.com>
2018-12-29 10:40:44 +0800

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,10 +8,24 @@ module.exports = {
     '@vue/standard'
   ],
   rules: {
+    //IDE default format
+    'indent': ['off'],
+    'eqeqeq': 'error',
+    'no-undef': 'off',
+    'new-cap': ['off'],
+    'no-unused-vars': 'error',
+    'object-curly-spacing': ['error', 'never'],
+    'semi': ['error', 'always', {'omitLastInOneLineBlock': true}],
+    'space-before-function-paren': [
+      'error',
+      {'anonymous': 'always', 'named': 'never', 'asyncArrow': 'always'}],
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // allow debugger during development
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
   }
-}
+};
